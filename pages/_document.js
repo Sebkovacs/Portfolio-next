d4838ae1de0b200c64047d3963a4d05a20022a59
Sebastian Kovacs <sebkovacs@gmail.com>
2022-05-10 23:44:46 +1000

    style mix up

--- pages/_document.js
+++ pages/_document.js
@@ -4,13 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
       <link rel="stylesheet" href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-      
-      <style>
-@import url('https://fonts.googleapis.com/css2?family=Oswald:wght@400;600&display=swap');
+      <link href="https://fonts.googleapis.com/css2?family=Long+Cang&family=Swanky+and+Moo+Moo&display=swap" rel="stylesheet" />
 
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-</style>
       </Head>
       <body>
         <Main />
